d7bb2dcc2c4e4beffc04c09456b99dd37f1b6822
maxemitchell <maxemitchell@gmail.com>
2022-03-10 23:53:03 -0600

    Lights and audio reaction work. Now need to put the hardware together and make the lights fancier

--- st_lighty.ino
+++ st_lighty.ino
@@ -1,16 +1,16 @@
-/******************************************************************************
- * Audio Reactive Lighting to be used at St. Patty's Day 2022 im Chicago
- *
- * Base Code from Sparkfun's Spectrum Shield Demo (Wes Furuya @ SparkFun Electronics and Toni Klopfenstein @SparkFun Electronics)
- * https://github.com/sparkfun/Spectrum_Shield
- * 
- * I am Max Mitchell
- * March 2022
- * 
- * use this at your own parties (only if I'm invited) :)
-*********************************************************************************/
-
 #include <FastLED.h>
+
+/******************************************************************************
+   Audio Reactive Lighting to be used at St. Patty's Day 2022 im Chicago
+
+   Base Code from Sparkfun's Spectrum Shield Demo (Wes Furuya @ SparkFun Electronics and Toni Klopfenstein @SparkFun Electronics)
+   https://github.com/sparkfun/Spectrum_Shield
+
+   I am Max Mitchell
+   March 2022
+
+   use this at your own parties (only if I'm invited) :)
+*********************************************************************************/
 
 // Declare audio pins
 #define STROBE 4
@@ -19,7 +19,7 @@
 #define DC_Right A1
 
 // Declare LED pins
-#define STRIP_1 7
+#define STRIP_1 8
 #define STRIP_2 8
 #define STRIP_3 9
 #define STRIP_4 10
@@ -39,7 +39,8 @@ int i;
 CRGB leds[NUM_STRIPS][NUM_LEDS];
 
 /********************Setup Loop*************************/
-void setup() {
+void setup()
+{
     // Set spectrum Shield pin configurations
     pinMode(STROBE, OUTPUT);
     pinMode(RESET, OUTPUT);
@@ -52,30 +53,35 @@ void setup() {
     delay(5);
 
     // Initialize LEDs
-    FastLED.addLeds<WS2812B, STRIP_1>(leds[0], NUM_LEDS);
-    FastLED.addLeds<WS2812B, STRIP_2>(leds[1], NUM_LEDS);
-    FastLED.addLeds<WS2812B, STRIP_3>(leds[2], NUM_LEDS);
-    FastLED.addLeds<WS2812B, STRIP_4>(leds[3], NUM_LEDS);
-    FastLED.addLeds<WS2812B, STRIP_5>(leds[4], NUM_LEDS);
-    FastLED.addLeds<WS2812B, STRIP_6>(leds[5], NUM_LEDS);
-    FastLED.addLeds<WS2812B, STRIP_7>(leds[6], NUM_LEDS);
+    FastLED.addLeds<WS2812B, STRIP_1, GRB>(leds[0], NUM_LEDS);
+    // FastLED.addLeds<WS2812B, STRIP_2, GRB>(leds[1], NUM_LEDS);
+    // FastLED.addLeds<WS2812B, STRIP_3, GRB>(leds[2], NUM_LEDS);
+    // FastLED.addLeds<WS2812B, STRIP_4, GRB>(leds[3], NUM_LEDS);
+    // FastLED.addLeds<WS2812B, STRIP_5, GRB>(leds[4], NUM_LEDS);
+    // FastLED.addLeds<WS2812B, STRIP_6, GRB>(leds[5], NUM_LEDS);
+    // FastLED.addLeds<WS2812B, STRIP_7, GRB>(leds[6], NUM_LEDS);
+
+    Serial.begin(9600);
 }
 
 /**************************Main Function Loop*****************************/
-void loop() {
+void loop()
+{
     Read_Frequencies();
     Graph_Frequencies();
 }
 
 /*******************Pull frquencies from Spectrum Shield********************/
-void Read_Frequencies() {
-//     digitalWrite(RESET, HIGH);
-//     delayMicroseconds(200);
-//     digitalWrite(RESET, LOW);
-//     delayMicroseconds(200);
+void Read_Frequencies()
+{
+    digitalWrite(RESET, HIGH);
+    delayMicroseconds(200);
+    digitalWrite(RESET, LOW);
+    delayMicroseconds(200);
 
     // Read frequencies for each band
-    for (freq_amp = 0; freq_amp < 7; freq_amp++) {
+    for (freq_amp = 0; freq_amp < 1; freq_amp++)
+    {
         digitalWrite(STROBE, HIGH);
         delayMicroseconds(50);
         digitalWrite(STROBE, LOW);
@@ -87,16 +93,29 @@ void Read_Frequencies() {
 }
 
 /*****************Print Out Band Values for Serial Plotter*****************/
-void Graph_Frequencies() {
-    for (int freq_band = 0; freq_band < 7; freq_band++) {
-        int lit_leds = floor((Frequencies_Left[freq_band] + Frequencies_Right[freq_band] / 2046) * NUM_LEDS);
+void Graph_Frequencies()
+{
+    for (int freq_band = 0; freq_band < 1; freq_band++)
+    {
+        int lit_leds = (Frequencies_Left[freq_band] + Frequencies_Right[freq_band]) / 2046. * NUM_LEDS;
 
-        for (int cur_led = 0; cur_led < lit_leds; cur_led++) {
-            leds[freq_band][cur_led] = CRGB::SpringGreen;
+        for (int cur_led = 0; cur_led < lit_leds; cur_led++)
+        {
+            leds[freq_band][cur_led] = CRGB::Green2;
         }
 
-        fadeToBlackBy(leds[freq_band], NUM_LEDS, 10);
+        fadeToBlackBy(leds[freq_band], NUM_LEDS, 20);
 
         FastLED.show();
+
+        Serial.print(lit_leds);
+        Serial.print("    ");
     }
+    Serial.println();
+
+    //  leds[0][0] = CRGB::Red;
+    //  leds[0][1] = CRGB::Green;
+    //  leds[0][2] = CRGB::Blue;
+
+    //  FastLED.show();
 }
